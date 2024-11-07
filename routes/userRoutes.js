c65979a6b0762ec2dcea81f6930030b5a8c730fd
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-07 14:00:45 -0500

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+
+// routes/userRoutes.js
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const { validateUser } = require('../middlewares/validateUser');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.get('/users', authMiddleware, userController.getAllUsers);
+router.get('/users/:id', authMiddleware, userController.getUserById);
+router.post('/users', validateUser, userController.createUser);
+router.put('/users/:id', authMiddleware, validateUser, userController.updateUser);
+router.delete('/users/:id', authMiddleware, userController.deleteUser);
+
+module.exports = router;
