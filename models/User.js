3dd0b6615f149f78500b3988bcda1e721cb593d0
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-07 13:23:54 -0500

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,35 @@
+
+// models/User.js
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  User.beforeCreate(async (user) => {
+    user.password = await bcrypt.hash(user.password, 10);
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Loan);
+  };
+
+  return User;
+};
