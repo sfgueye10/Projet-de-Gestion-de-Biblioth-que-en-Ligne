8ee3c86efeaf09788e03f4f6c687404919f482d8
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:52:30 -0500

    Update Role.js

--- models/Role.js
+++ models/Role.js
@@ -1,21 +1,4 @@
 // models/Role.js
-<<<<<<< HEAD
-module.exports = (sequelize, DataTypes) => {
-    const Role = sequelize.define('Role', {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    });
-  
-    Role.associate = (models) => {
-      Role.hasMany(models.User, { foreignKey: 'roleId' });
-    };
-  
-    return Role;
-  };
-  
-=======
 export default (sequelize, DataTypes) => {
   const Role = sequelize.define('Role', {
     roleID: {
@@ -34,4 +17,3 @@ export default (sequelize, DataTypes) => {
 
   return Role;
 };
->>>>>>> fallou
