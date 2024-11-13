f7ad71b87df0aca10507f2516bebe06b4cea9b63
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:59:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const { sequelize } = require('./models');
-const userRoutes = require('./routes/userRoutes');
-const bookRoutes = require('./routes/bookRoutes');
-const authorRoutes = require('./routes/authorRoutes');
-const loanRoutes = require('./routes/loanRoutes');
-const genreRoutes = require('./routes/genreRoutes');
-const commentRoutes = require('./routes/commentRoutes');
-const reservationRoutes = require('./routes/reservationRoutes');
-const roleRoutes = require('./routes/roleRoutes');
-const authRoutes = require('./routes/authRoutes');
-const authMiddleware = require('./middlewares/authMiddleware');
-
-const app = express();
-app.use(express.json());
-
-app.use('/api', authRoutes);
-app.use('/api', authMiddleware, userRoutes);
-app.use('/api', authMiddleware, bookRoutes);
-app.use('/api', authMiddleware, authorRoutes);
-app.use('/api', authMiddleware, loanRoutes);
-app.use('/api', authMiddleware, genreRoutes);
-app.use('/api', authMiddleware, commentRoutes);
-app.use('/api', authMiddleware, reservationRoutes);
-app.use('/api', authMiddleware, roleRoutes);
-
-const PORT = process.env.PORT || 5000;
-
-sequelize.sync({ force: true }).then(() => {
-  console.log('Database & tables created!');
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-=======
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
@@ -76,5 +41,4 @@ db.sequelize.sync({ force: false }).then(() => {
   });
 }).catch(err => {
   console.error('Erreur lors de la synchronisation des tables :', err);
->>>>>>> fallou
 });
