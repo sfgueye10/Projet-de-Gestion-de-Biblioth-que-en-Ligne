dc543387ce9b50d92aefa545882a66c26cfc8980
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:43:01 -0500

    Update reservationRoutes.js

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const reservationController = require('../controllers/reservationController');
-const { validateReservation } = require('../middlewares/validateReservation');
-
-router.get('/reservations', reservationController.getAllReservations);
-router.get('/reservations/:id', reservationController.getReservationById);
-router.post('/reservations', validateReservation, reservationController.createReservation);
-router.put('/reservations/:id', validateReservation, reservationController.updateReservation);
-router.delete('/reservations/:id', reservationController.deleteReservation);
-
-module.exports = router;
-=======
 // routes/reservationRoutes.js
 import express from 'express';
 import {
@@ -40,4 +26,3 @@ router.put('/reservations/:id', updateReservation);
 router.delete('/reservations/:id', deleteReservation);
 
 export default router;
->>>>>>> fallou
