e7e7edad332fe538bc82749ac275cbda54bc8a53
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:49:36 -0500

    Update Genre.js

--- models/Genre.js
+++ models/Genre.js
@@ -1,25 +1,4 @@
 // models/Genre.js
-<<<<<<< HEAD
-module.exports = (sequelize, DataTypes) => {
-    const Genre = sequelize.define('Genre', {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-    });
-  
-    Genre.associate = (models) => {
-      Genre.hasMany(models.Book, { foreignKey: 'genreId' });
-    };
-  
-    return Genre;
-  };
-  
-=======
 export default (sequelize, DataTypes) => {
   const Genre = sequelize.define('Genre', {
     genreID: {
@@ -40,4 +19,3 @@ export default (sequelize, DataTypes) => {
 
   return Genre;
 };
->>>>>>> fallou
