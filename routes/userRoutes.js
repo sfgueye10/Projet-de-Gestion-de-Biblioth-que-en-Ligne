f7afed5745362f96f2694d7117cf9ccc457082c1
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:44:23 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,4 @@
 // routes/userRoutes.js
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const { validateUser } = require('../middlewares/validateUser');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get('/users', authMiddleware, userController.getAllUsers);
-router.get('/users/:id', authMiddleware, userController.getUserById);
-router.post('/users', validateUser, userController.createUser);
-router.put('/users/:id', authMiddleware, validateUser, userController.updateUser);
-router.delete('/users/:id', authMiddleware, userController.deleteUser);
-
-module.exports = router;
-=======
 import express from 'express';
 import { body } from 'express-validator';
 import {
@@ -53,4 +38,3 @@ router.put('/users/:id', [
 router.delete('/users/:id', authenticateToken, deleteUser);
 
 export default router;
->>>>>>> fallou
