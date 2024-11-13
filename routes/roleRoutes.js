47658d56e9b6b6953866d8bcd7cec9fbd7d063f4
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:43:37 -0500

    Update roleRoutes.js

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const roleController = require('../controllers/roleController');
-const { validateRole } = require('../middlewares/validateRole');
-
-router.get('/roles', roleController.getAllRoles);
-router.get('/roles/:id', roleController.getRoleById);
-router.post('/roles', validateRole, roleController.createRole);
-router.put('/roles/:id', validateRole, roleController.updateRole);
-router.delete('/roles/:id', roleController.deleteRole);
-
-module.exports = router;
-=======
 // routes/roleRoutes.js
 import express from 'express';
 import {
@@ -40,4 +26,3 @@ router.put('/roles/:id', updateRole);
 router.delete('/roles/:id', deleteRole);
 
 export default router;
->>>>>>> fallou
