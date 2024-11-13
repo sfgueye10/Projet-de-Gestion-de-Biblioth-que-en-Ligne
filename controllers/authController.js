04bb72477b602520876e6202228c59e614d88642
sfgueye10 <2715320@collegelacite.ca>
2024-11-13 15:53:15 -0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-// controllers/authController.js
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ where: { email } });
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    }
-
-    const token = jwt.sign({ id: user.id, email: user.email }, 'your_jwt_secret', { expiresIn: '1h' });
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-=======
 import { check, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
@@ -84,4 +57,3 @@ export const login = [
     }
   }
 ];
->>>>>>> fallou
