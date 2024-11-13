f59b22468259a57e20bd6ac7e1113b08191fe799
serignemb <161747109+serignemb@users.noreply.github.com>
2024-11-13 15:51:41 -0500

    Update Reservation.js

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,30 +1,4 @@
 // models/Reservation.js
-<<<<<<< HEAD
-module.exports = (sequelize, DataTypes) => {
-    const Reservation = sequelize.define('Reservation', {
-      reservationDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      bookId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    });
-  
-    Reservation.associate = (models) => {
-      Reservation.belongsTo(models.User, { foreignKey: 'userId' });
-      Reservation.belongsTo(models.Book, { foreignKey: 'bookId' });
-    };
-  
-    return Reservation;
-  };
-  
-=======
 export default (sequelize, DataTypes) => {
   const Reservation = sequelize.define('Reservation', {
     reservationID: {
@@ -59,4 +33,3 @@ export default (sequelize, DataTypes) => {
   return Reservation;
 };
 
->>>>>>> fallou
